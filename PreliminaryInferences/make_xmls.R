a718cb612bfea42e92f71c9b3c34acef0ed02bd1
sebastian duchene <sebastianduchene@sebastiansair2.mobility.unimelb.net.au>
2020-01-03 16:41:18 +1100

    updated xml maker in R

--- PreliminaryInferences/make_xmls.R
+++ PreliminaryInferences/make_xmls.R
@@ -24,35 +24,34 @@ SEQ_DATA
 template <- readLines('template.xml')
 fasta_files <- dir(pattern = 'fasta$')
 
-i <- 1
-# Write for loop here to set up all the fasta files:
+# To make xml for all fasta files using model in template.xml
 
-temp_aln <- read.dna(fasta_files[i], format = 'fasta')
-outfile <- gsub('.fasta', '', fasta_files[i])
-temp_splits <- strsplit(rownames(temp_aln), '_')
-taxon_names <- rownames(temp_aln)
-secs <- sapply(1:nrow(temp_aln), function(x) toupper(paste(as.character(temp_aln[x, ]), collapse = '')))
-dates <- sapply(temp_splits, function(x) x[[4]])
-x_coord <- sapply(temp_splits, function(x) x[[2]])
-y_coord <- sapply(temp_splits, function(x) x[[3]])
+for(i in 1:length(fasta_files)){
+      temp_aln <- read.dna(fasta_files[i], format = 'fasta')
+      outfile <- gsub('.fasta', '', fasta_files[i])
+      temp_splits <- strsplit(rownames(temp_aln), '_')
+      taxon_names <- rownames(temp_aln)
+      secs <- sapply(1:nrow(temp_aln), function(x) toupper(paste(as.character(temp_aln[x, ]), collapse = '')))
+      dates <- sapply(temp_splits, function(x) x[[4]])
+      x_coord <- sapply(temp_splits, function(x) x[[2]])
+      y_coord <- sapply(temp_splits, function(x) x[[3]])
 
-
-temp_taxon_block <- paste(sapply(1:nrow(temp_aln), function(x) gsub('Y_COORD', y_coord[x],
+      temp_taxon_block <- paste(sapply(1:nrow(temp_aln), function(x) gsub('Y_COORD', y_coord[x],
 		 gsub('X_COORD', x_coord[x],
 		 gsub('DATE', dates[x],
 		 gsub('TAXON_NAME', taxon_names[x], taxon_block))))),
 		 collapse = '\n')
 
-#<!--INPUT_TAXA-->
-temp_aln_block <- paste(sapply(1:nrow(temp_aln), function(x) gsub('SEQ_DATA', secs[x],
+      #<!--INPUT_TAXA-->
+      temp_aln_block <- paste(sapply(1:nrow(temp_aln), function(x) gsub('SEQ_DATA', secs[x],
 	       	  gsub('TAXON_NAME', taxon_names[x], seq_block))), collapse = '\n')
 
-#<!--INPUT_ALIGNMENT-->
-temp_xml <- gsub('OUT_FILE', outfile,
+      #<!--INPUT_ALIGNMENT-->
+      temp_xml <- gsub('OUT_FILE', outfile,
 	 gsub('<!--INPUT_ALIGNMENT-->', temp_aln_block,
 	 gsub('<!--INPUT_TAXA-->', temp_taxon_block, template)))
 
-cat(temp_xml, file = gsub('fasta', 'xml', fasta_files[i]), sep = '\n')
-
+      cat(temp_xml, file = gsub('fasta', 'xml', fasta_files[i]), sep = '\n')
+}
 
 
