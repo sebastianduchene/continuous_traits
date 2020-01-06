497949823f3a60d28f66b2bba7e2f98318b975b4
Leo <58927485+LeoFeatherstone@users.noreply.github.com>
2020-01-06 11:33:13 +1100

    Updates 100 tips trees with 1&2 traits and internal node states saved

--- test_sequences/1trait_100tips/1trait_100tips_MVNsim.R
+++ test_sequences/1trait_100tips/1trait_100tips_MVNsim.R
@@ -1,4 +1,4 @@
-# 2 traits, 100 tips
+# 1 trait, 100 tips
 # Store Branch lengths in BM space too
 
 library(ape,NELSI)
@@ -8,7 +8,7 @@ library(phytools)
 library(MASS)
 library(phangorn)
 
-setwd("~/Documents/GitHub/continuous_traits/test_sequences/2traits_100tips/")
+setwd("~/Documents/GitHub/continuous_traits/test_sequences/1trait_100tips/")
 #tr <- sim.bdsky.stt(n = 100, lambdasky = 2, deathsky = 1, sampprobsky = 0.1, timesky = 0)[[1]]
 #write.tree(tr, file = "ReferenceTree_100tips_labmda2_death1_samprob0.1.nwk")
 tr <- read.tree("ReferenceTree_100tips_labmda2_death1_samprob0.1.nwk")
@@ -19,11 +19,11 @@ tiplabs <- tr$tip.label
 # 100 tips => 2N-2  - 198 (exclude root) 
 # But, we assume the root node is fixed at zero so it is not an RV => 198 measurements for each trait
 
-mu <- matrix(c(rep(0,198),rep(0,198)), ncol = 1)
-R <- matrix(c(1, 0, 0, 1), 2)
+mu <- rep(0,198)
+R <- 1
 C <- vcvPhylo(tr, anc.nodes = T)
 V <- kronecker(R, C)
-Ntraits <- 2
+Ntraits <- 1
 Ntips <- length(tr$tip.label)
 Nnodes <- 198
 delta_trait <- list()
@@ -32,7 +32,6 @@ traits <- list()
 
 sim_traits <- mvrnorm(n = 5, mu = mu, Sigma = V)
 
-# Break into Ntraits = 2 trait matrices of p obs x n nodes. Store in lists. Each element of the list is the matrix for a given trait
 
 for (i in 1:Ntraits) {
   traits[[i]] <- sim_traits[,((i-1)*Nnodes+1):(Nnodes*i)]
@@ -47,9 +46,9 @@ for (i in 1:length(traits)){
 }
 
 # Saving Node states
-node_states <- cbind(c(1:5), cbind(traits[[1]], traits[[2]]))
-colnames(node_states) <- c("replicate", paste0("t1_node", 1:199), paste0("t2_node", 1:199))
-write.csv(node_states, row.names = F, file = "2trait2_100tips_nodestates.csv")
+node_states <- cbind(c(1:5), traits[[1]])
+colnames(node_states) <- c("replicate", paste0("t1_node", 1:199))
+write.csv(node_states, row.names = F, file = "1trait_100tips_nodestates.csv")
 
 # Getting delta trait per branch using edge objct
 v1 <- tr$edge[,1]
@@ -75,37 +74,26 @@ for (t in 1:length(traits)){
 
 # NB: Number of columns is now 199 instead of 198 (2N-1 instead of 2N-2)
 
-# Euclidean Distance
-distance <- sqrt((delta_trait[[1]])^2 + (delta_trait[[2]])^2)
-
 # Diffusivity (D)
 Path <- (sum(tr$edge.length))
-D <-  rowSums(distance)/Path
+D <-  rowSums(abs(delta_trait[[1]]))/Path
 
 # Generating test sequences and saving with delta traits
+DELTA_TRAITS <- cbind(paste0("N", 1:5), delta_trait[[1]])
+colnames(DELTA_TRAITS) <- c("replicate", paste0("t1_node", 1:199))
 
-DELTA_TRAITS <- matrix()
+write.csv(DELTA_TRAITS, row.names = F, file = "branch_delta_100tips_1trait.csv")
 
-for (t in 1:(length(traits)-1)){
-  DELTA_TRAITS <- cbind(delta_trait[[t]], delta_trait[[t+1]])
-}
-
-colnames(DELTA_TRAITS) <- c(paste0("t1_node", 1:199), paste0("t2_node", 1:199))
-write.csv(cbind(paste0("N", 1:5), DELTA_TRAITS), row.names = F, file = "branch_delta_100tips_2traits.csv")
-
-tr$tip.label <- paste0(tiplabs, '_', round(traits[[1]][N,1:Ntips],2), '_', round(traits[[2]][N,1:Ntips],2), '_', 
+tr$tip.label <- paste0(tiplabs, '_', round(traits[[1]][N,1:Ntips],2), '_', 
                        round(allnode.times(tr, tipsonly = T, reverse = F), 2))
 
 for (N in 1:length(sim_traits[,1])){
-
+  
   phylogram <- tr
-
   
   phylogram$edge.length <- phylogram$edge.length * 1E-2
   aln <- as.DNAbin(simSeq(phylogram, l = 5000))
   
-  corr <- R[1,2]/(sqrt(R[1,1]*R[2,2]))
-  
-  write.dna(aln, file = paste0("BMtest2traits_sigma", R[1,1], '_', R[2,2],"corr", corr, '_', corr, 'D', round(D[N],2),'_100tips','N', N, '.fasta'), 
+  write.dna(aln, file = paste0("BMtest1trait_sigma", R, 'D', round(D[N],2),'_100tips','N', N, '.fasta'), 
             format = 'fasta', nbcol = -1, colsep = '')
 }
